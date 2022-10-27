4db2ef2793c5fcd945ff348b28dff84584fad423
shathipaul <shathipaul11@gmail.com>
2022-10-27 19:07:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { request } = require('express');
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const categories = require('./data/categories.json');
+const languageDetail = require('./data/languageDetail.json')
+
+app.get('/', (req, res) => {
+    res.send('language API Running');
+})
+
+app.get('/language-categories', (req, res) =>{
+    res.send(categories);
+})
+
+// app.get('/languageDetail/:id', (req, res) =>{
+//     const id = req.params.id;
+//     const selectedLanguage = languageDetail.find( language => language.id === id)
+//     res.send(selectedLanguage);
+// })
+app.get('/languageDetail/', (req, res) =>{
+    res.send(languageDetail);
+})
+
+app.listen(port, () => {
+    console.log('Learn language server running', port);
+})
